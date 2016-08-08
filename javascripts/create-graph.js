b118c463842c40d508b7b44f1f7736394807cb14
huyle333 <huyle333@bu.edu>
2016-08-07 18:28:40 -0700

    [HL] Basic graph demonstration.

--- javascripts/create-graph.js
+++ javascripts/create-graph.js
@@ -1,13 +1,52 @@
-function createGraph(data) {
-	
-}
-
-function parseData(callback) {
-	Papa.parse("../data/data.csv", {
+/*
+ * Parse the data and create a graph with the data.
+ */
+function parseData(createGraph) {
+	Papa.parse("../data/spanish-silver.csv", {
 		download: true,
 		complete: function(results) {
-			callback(results.data);
+			createGraph(results.data);
 		}
+	});
+}
+
+function createGraph(data) {
+	var years = [];
+	var silverMinted = ["Silver Minted"];
+
+	for (var i = 1; i < data.length; i++) {
+		years.push(data[i][0]);
+		silverMinted.push(data[i][2]);
+	}
+
+	console.log(years);
+	console.log(silverMinted);
+
+	var chart = c3.generate({
+		bindto: '#chart',
+	    data: {
+	        columns: [
+	        	silverMinted
+	        ]
+	    },
+	    axis: {
+	        x: {
+	            type: 'category',
+	            categories: years,
+	            tick: {
+	            	multiline: false,
+                	culling: {
+                    	max: 15
+                	}
+            	}
+	        }
+	    },
+	    zoom: {
+        	enabled: true
+    	},
+	    legend: {
+	        position: 'right'
+	    }
 	});
 }
 
